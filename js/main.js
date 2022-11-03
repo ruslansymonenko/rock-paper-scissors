f281e5ad5682ce8c29400e8940ddca0426171718
Ruslan Symonenko <symonenkors@gmail.com>
2022-11-03 17:32:00 +0200

    adding selected features to the playing field

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const moveBtns = document.querySelectorAll('.move__btn');
+    const playBtn = document.querySelector('.play__btn');
+    const playerResult = document.querySelector('#playerResult');
+    const compResult = document.querySelector('#compResult');
+    const choosenMovePlayer = document.querySelector('.choosen__move-player');
+    const choosenMoveComp = document.querySelector('.choosen__move-comp');
+
+    const gameResults = {
+        player: 0,
+        comp: 0
+    };
+
+    const gameOption = ['rock', 'paper', 'scissors'];
 
 
     function setPlayerChoose (btn) {
@@ -17,11 +29,26 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     };
 
+    function setGamePicture (option, field) {
+        switch (option) {
+            case 'rock':
+                field.setAttribute('src', './assets/btn-rock.png')
+                break
+            case 'paper':
+                field.setAttribute('src', './assets/btn-paper.png')
+                break
+        }
+    }
 
     moveBtns.forEach(item => {
         item.addEventListener('click', (e) => {
             let playerChoose = setPlayerChoose(item);
             changeBtnsStyle(moveBtns, playerChoose);            
         });
+    });
+
+    playBtn.addEventListener('click', () => {
+        setGamePicture('rock', choosenMovePlayer);
+        setGamePicture('paper', choosenMoveComp);
     })
 })
